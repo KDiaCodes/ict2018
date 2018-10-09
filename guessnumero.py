780613453890b35616ba4459ff6e1a2898672705
KDiaCodes <KDiaCodes@users.noreply.github.com>
2018-10-09 14:47:39 +0200

    Update guessnumero.py

--- guessnumero.py
+++ guessnumero.py
@@ -4,21 +4,25 @@ import os
 lives = 6
 hidden = random.randrange(1, 100)
 print(hidden)
-CheckNumber()
+while lives > 0:
+    guess = int(input("Please enter your guess: "))
+    if guess == hidden:
+        print("Hit!")
+        input("Press enter to close")
+        os.system(r'start C:\Windows\System32\cmd.exe /c C:\Users\KENRICSA\Desktop\start.bat')
+        break
+    elif guess < hidden:
+        print("Your guess is too low")
+        lives = lives - 1
+        print("lives left: [",lives,"]")
+    else:
+        print("Your guess is too high")
+        lives = lives - 1
+        print("lives left: [",lives,"]")
 
-def CheckNumber():
-    while lives > 0:
-        guess = int(input("Please enter your guess: "))
-        if guess == hidden:
-            print("Hit!")
-            input("Press enter to close")
-            os.system(r'start C:\Windows\System32\cmd.exe /c C:\Users\KENRICSA\Desktop\start.bat')
-        elif guess < hidden:
-            print("Your guess is too low")
-        else:
-            lives = lives - 1
-            print("Your guess is too high")
-            MakeAGuess(life)
+
+
+if lives == 0:
     print("you lose")
     input("Press enter to close")
     os.system(r'start C:\Windows\System32\cmd.exe /c C:\Users\KENRICSA\Desktop\start.bat')
