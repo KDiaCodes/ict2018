5c7d12d9a5f7f314be7b84de5eeb4d7fe2f3a949
KDiaCodes <KDiaCodes@users.noreply.github.com>
2018-10-19 08:33:54 +0200

    Add files via upload

---
+++ intwentyfifty.py
@@ -0,0 +1,8 @@
+name = input("Hello, what is your name? ")
+
+age = int(input("What year where you born? "))
+
+yearsold = 2050 - age
+
+print("in 2050 you will be " + str(yearsold) + " years old")
+input("Press enter to leave")
--- start.bat
+++ start.bat
@@ -16,10 +16,7 @@ echo [5] factorial
 echo [6] sort
 echo [7] guess numero
 echo [8] counting days
-echo [9] Instructables - A How-To Website
-echo [10] YouTube - Online Videos
-echo [11] Answers - Online Encyclopedia
-echo [12] Wikipedia - Online Encyclopedia
+echo [9] 2050
 echo.
 echo [e] Exit
 echo.
@@ -38,10 +35,7 @@ if %udefine%==5 CMD /c python factorial.py
 if %udefine%==6 CMD /c python sortarray.py
 if %udefine%==7 CMD /c python guessnumero.py
 if %udefine%==8 CMD /c python countingdays.py
-if %udefine%==9 start www.instructables.com
-if %udefine%==10 start www.youtube.com
-if %udefine%==11 start www.answers.com
-if %udefine%==12 start www.wikipedia.com
+if %udefine%==9 CMD /c python intwentyfifty.py
 if %udefine%==e goto exit
 
 exit
